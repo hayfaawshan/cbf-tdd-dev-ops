e7ddb67cbb8ab663b0404f0c7047a2d06298aabe
Hayfa <hayfaawshan@gmail.com>
2021-09-23 19:26:07 +0100

    FizzBuzz kata challenge.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-export const projectName = () => {
-  return 'Kata starter project';
-};
+export const FizzBuzzer = (number) =>
+	number % 15 === 0
+		? "FizzBuzz"
+		: number % 5 === 0
+		? "Buzz"
+		: number % 3 === 0
+		? "Fizz"
+		: number;
